2e1e4e0469eb6b06afbb03f9ee282de739a5eaa1
Cristian Somma <12999453@itisrossi.vi.it>
2025-02-28 11:43:24 +0100

    Add files via upload

---
+++ ES-06-03-25/ES-06-03-25/ES1.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+
+namespace ES_06_03_25
+{
+    internal class ES1
+    {
+        public static void es1()
+        {
+            Random random = new Random(Environment.TickCount);
+            int arrayLenght;
+
+            WriteLine("Benvenuto\nInserisci la quantità di numeri da generare:");
+            Write("> ");
+
+            while (!int.TryParse(ReadLine(), out arrayLenght))
+            {
+                WriteLine("ERRORE: Inserire un valore valido");
+                Write("> ");
+            }
+
+            int[] randomNums = new int[arrayLenght];
+            int[] occurancesCounter = new int[21];
+            Array.Fill(occurancesCounter, 0);
+
+            Write("Numeri Generati: ");
+            for (int i = 0; i < randomNums.Length; i++)
+            {
+                randomNums[i] = random.Next(0, 21);
+                occurancesCounter[randomNums[i]]++;
+                Write($" {randomNums[i]} |");
+            }
+
+            WriteLine($"\n{Array.IndexOf(occurancesCounter, occurancesCounter.Max())} è apparso {occurancesCounter.Max()} volte");
+
+        }
+    }
+}
---
+++ ES-06-03-25/ES-06-03-25/Program.cs
@@ -0,0 +1,84 @@
+﻿using static System.Console;
+
+namespace ES_06_03_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(Environment.TickCount);
+            int arrayLenght;
+
+            WriteLine("Benvenuto\nInserisci la quantità di numeri da generare:");
+            Write("> ");
+
+            while (!int.TryParse(ReadLine(), out arrayLenght))
+            {
+                WriteLine("ERRORE: Inserire un valore valido");
+                Write("> ");
+            }
+
+            int[] randomNums = new int[arrayLenght];
+
+            for(int i = 0;  i < arrayLenght; i++)
+            {
+                randomNums[i] = random.Next(101);
+            }
+
+            int[] firstHalf = Sort(Split(randomNums, 0, arrayLenght / 2), false);
+            int[] secondHalf = Sort(Split(randomNums, arrayLenght/2, arrayLenght), true);
+
+            Write("\nArray completo: ");
+            PrintArray(randomNums);
+
+            Write("\nPrima metà dell'array ordinato: ");
+            PrintArray(firstHalf);
+
+            Write("\nSeconda metà dell'array in ordine inverso: ");
+            PrintArray(secondHalf);
+        }
+
+        static int[] Split(int[] array, int startingValue, int endValue)
+        {
+            int[] splittedArray = new int[endValue-startingValue];
+            int counter = 0;
+
+            for(int i = startingValue; i<endValue; i++)
+            {
+                splittedArray[counter] = array[i];
+                counter++;
+            }
+
+            return splittedArray;
+        }
+        
+        static int[] Sort(int[] array, bool isIncreasing)
+        {
+            int[] processedArray = new int[array.Length];
+            int lastElement = array[0];
+            processedArray[0] = lastElement;
+            for (int i = 1; i < array.Length; i++)
+            {
+                processedArray[i] = array[i];
+                if (array[i] > lastElement)
+                {
+                    processedArray[i - 1] = array[i];
+                    processedArray[i] = lastElement;
+                }
+            }
+
+            if (isIncreasing)
+                Array.Reverse(processedArray);
+
+            return processedArray;
+        }
+
+        static void PrintArray(int[] array)
+        {
+            foreach(int element in array)
+            {
+                Write($"{element} - ");
+            }
+        }
+    }
+}
